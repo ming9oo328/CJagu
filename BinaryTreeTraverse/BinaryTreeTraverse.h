56e5fe7eb00a725ab0da0258ca8a2e57152d1e84
ming9oo <mj10050203@gmail.com>
2023-02-03 09:55:02 +0900

    BinaryTreeTraverse_수정

--- BinaryTreeTraverse/BinaryTreeTraverse.h
+++ BinaryTreeTraverse/BinaryTreeTraverse.h
@@ -21,4 +21,10 @@ BTreeNode* GetRightSubTree(BTreeNode* bt);
 void MakeLeftSubTree(BTreeNode* main, BTreeNode* sub);
 void MakeRightSubTree(BTreeNode* main, BTreeNode* sub);
 
+typedef void VisitFuncPtr(BTData data);
+
+void PreorderTraverse(BTreeNode* bt, VisitFuncPtr action);
+void InorderTraverse(BTreeNode* bt, VisitFuncPtr action);
+void PostorderTraverse(BTreeNode* bt, VisitFuncPtr action);
+
 #endif
